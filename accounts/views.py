fcf9b329c21e15b48535b32ce82c3913391e410f
Simon Tidman <simontidman@Simons-MBP.lan>
2020-11-30 22:14:41 +0000

    added accounts

--- accounts/views.py
+++ accounts/views.py
@@ -1,3 +1,104 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect, reverse
+from django.contrib import auth, messages
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth.models import User
+from .forms import UserLoginForm, UserRegistrationForm
+from .forms import ProfileForm
 
-# Create your views here.
+from .models import Profile
+from datetime import datetime
+from datetime import date
+
+from django.template.context_processors import csrf
+
+from django.views.generic import TemplateView
+
+
+def login(request):
+    """Return a login page"""
+
+    return redirect('index')
+
+
+def index(request):
+
+    if request.user.is_authenticated:
+        return redirect(reverse('home'))
+    if request.method == 'POST':
+        login_form = UserLoginForm(request.POST)
+
+        if login_form.is_valid():
+            username = request.POST['username']
+            password = request.POST['password']
+            user = auth.authenticate(username=username,
+                                     password=password)
+
+            if user is not None:
+                auth.login(request=request, user=user)
+                messages.error(request, "You have successfully logged in")
+                return redirect(reverse('home'))
+            else:
+
+                messages.error(request, "oops")
+                messages.error(request, user)
+                # redirects to switcher instead of dash to set group and business
+
+    else:
+        login_form = UserLoginForm()
+
+    return render(request, 'index.html', {'login_form': login_form})
+
+
+def logout(request):
+    """Log the user out"""
+    auth.logout(request)
+    messages.success(request, "You have successfully been logged out")
+    return redirect('index')
+
+
+@login_required
+def registration(request):
+
+    if request.method == "POST":
+        registration_form = UserRegistrationForm(request.POST)
+        profile_form = ProfileForm(request.POST, request.FILES)
+        telephone = request.POST.get("telephone", "default")
+        image = request.FILES.get("profile_image")
+
+        if registration_form.is_valid() and profile_form.is_valid():
+            xe = registration_form.save()
+            xe.profile.telephone = telephone
+            xe.profile.profile_image = image
+            xe.save()
+            return redirect(reverse('new_business'))
+
+    registration_form = UserRegistrationForm()
+    profile_form = ProfileForm()
+    return render(request, 'registration.html', {'registration_form': registration_form, 'profile_form': profile_form})
+
+
+@login_required
+def user_profile(request):
+    """the users profile page"""
+    instance = Profile.objects.get(pk=request.user.pk)
+    if request.method == "POST":
+
+        form = ProfileForm(request.POST, request.FILES, instance=instance)
+        if form.is_valid():
+            form.save(commit=True)
+
+        return redirect(reverse('home'))
+        messages.error(request, "Profile Updated")
+
+    profile = ProfileForm(instance=instance)
+    return render(request, 'profile.html', {'profile': profile, 'instance': instance})
+
+
+# class ServiceWorkerView(TemplateView):
+#     template_name = 'sw.js'
+#     content_type = 'application/javascript'
+#     name = 'sw.js'
+
+
+def error(request):
+    return render(request, 'error.html')
--- ihorse/settings.py
+++ ihorse/settings.py
@@ -40,6 +40,11 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'accounts',
+    'password_reset',
+    'bootstrap4',
+    'django_forms_bootstrap',
+    'crispy_forms',
+    'home',
 ]
 
 MIDDLEWARE = [
@@ -57,7 +62,7 @@ ROOT_URLCONF = 'ihorse.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [os.path.join(BASE_DIR, 'templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -65,12 +70,18 @@ TEMPLATES = [
                 'django.template.context_processors.request',
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
+                'django.template.context_processors.media',
             ],
         },
     },
 ]
 
 WSGI_APPLICATION = 'ihorse.wsgi.application'
+
+AUTHENTICATION_BACKENDS = [
+    'django.contrib.auth.backends.ModelBackend',
+    'accounts.backends.EmailAuth'
+]
 
 
 # Database
@@ -121,3 +132,11 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = '/static/'
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, "static"),
+)
+MEDIA_URL = '/media/'
+MEDIAFILES_LOCATION = 'media'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+
+CRISPY_TEMPLATE_PACK = 'bootstrap4'

28fbc61bfe4a6ffebeae7c60a0dc45f20c503296
Simon Tidman <simontidman@Simons-MBP.lan>
2020-12-20 22:47:19 +0000

    removed typpo from urls main

--- ihorse/urls.py
+++ ihorse/urls.py
@@ -15,7 +15,7 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
-from home.views import index, ServiceWorkerView
+from home.views import ServiceWorkerView
 
 
 urlpatterns = [

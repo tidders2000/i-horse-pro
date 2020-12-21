93e58d01b20e9a938d7e9d3c4eb5748ef770b15c
Simon Tidman <simontidman@Simons-MBP.lan>
2020-12-21 14:34:51 +0000

    removed send stuff

--- templates/sworker.js
+++ templates/sworker.js
@@ -1,6 +1,6 @@
 importScripts('https://storage.googleapis.com/workbox-cdn/releases/5.0.0/workbox-sw.js');
 
-const VERSION = '2.4';
+const VERSION = '2.6';
 
 if (workbox) {
     console.log(`Yay! Workbox is loaded 🎉`);
@@ -64,20 +64,20 @@ workbox.routing.setCatchHandler(({ event }) => {
 });
 
 // Register event listener for the 'push' event.
-self.addEventListener('push', function(event) {
-    // Retrieve the textual payload from event.data (a PushMessageData object).
-    // Other formats are supported (ArrayBuffer, Blob, JSON), check out the documentation
-    // on https://developer.mozilla.org/en-US/docs/Web/API/PushMessageData.
-    const eventInfo = event.data.text();
-    const data = JSON.parse(eventInfo);
-    const head = data.head || 'New Notification 🕺🕺';
-    const body = data.body || 'This is default content. Your notification didn\'t have one 🙄🙄';
+// self.addEventListener('push', function(event) {
+//     // Retrieve the textual payload from event.data (a PushMessageData object).
+//     // Other formats are supported (ArrayBuffer, Blob, JSON), check out the documentation
+//     // on https://developer.mozilla.org/en-US/docs/Web/API/PushMessageData.
+//     const eventInfo = event.data.text();
+//     const data = JSON.parse(eventInfo);
+//     const head = data.head || 'New Notification 🕺🕺';
+//     const body = data.body || 'This is default content. Your notification didn\'t have one 🙄🙄';
 
-    // Keep the service worker alive until the notification is created.
-    event.waitUntil(
-        self.registration.showNotification(head, {
-            body: body,
-            icon: 'https://i.imgur.com/MZM3K5w.png'
-        })
-    );
-});
+//     // Keep the service worker alive until the notification is created.
+//     event.waitUntil(
+//         self.registration.showNotification(head, {
+//             body: body,
+//             icon: 'https://i.imgur.com/MZM3K5w.png'
+//         })
+//     );
+// });

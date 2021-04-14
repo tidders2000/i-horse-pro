7ab5d876fb5933dbd906b77a9d180d538ee18e7c
Simon Tidman <simontidman@Simons-MBP-2.lan>
2021-04-14 22:13:30 +0100

    updated sw with workbox

--- templates/sworker.js
+++ templates/sworker.js
@@ -1,77 +1,80 @@
-// var CACHE_STATIC_NAME = 'static-v2';
-// var CACHE_DYNAMIC_NAME = 'dynamic-v1';
 
-// self.addEventListener('install', function(event) {
-//     console.log('[Service Worker] Installing Service Worker ...', event);
-//     event.waitUntil(
-//         caches.open(CACHE_STATIC_NAME)
-//         .then(function(cache) {
-//             console.log('[Service Worker] Precaching App Shell');
-//             cache.addAll([
-//                 '/accounts/error/',
+importScripts('https://storage.googleapis.com/workbox-cdn/releases/6.1.1/workbox-sw.js');
 
-//                 // '/home/templates/home.html',
+const VERSION = '2.0';
+console.log('Hello from service-worker.js');
 
-//                 '/home/',
+const {registerRoute} = workbox.routing;
+const {CacheFirst} = workbox.strategies;
+const {NetworkFirst} = workbox.strategies;
+const {CacheableResponse} = workbox.cacheableResponse;
+const {StaleWhileRevalidate} = workbox.strategies;
+const {CacheableResponsePlugin} = workbox.cacheableResponse;
+const {ExpirationPlugin} = workbox.expiration;
+const {precacheAndRoute} =workbox.precaching
 
-
-//                 // '/templates/base.html',
-//                 // '/templates/basePW.html',
-//                 // 'https://i-horse.s3.amazonaws.com/static/images/logo5.png',
-//                 // 'https://i-horse.s3.amazonaws.com/static/css/base.css',
-//                 // 'https://cdnjs.cloudflare.com/ajax/libs/materialize/0.100.2/css/materialize.min.css',
-//                 // // 'https://cors-anywhere.herokuapp.com/https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css',
-//                 // 'https://i-horse.s3.amazonaws.com/static/images/logo/mobile_logo.png'
+precacheAndRoute([
+    // {url: '/home/', revision: '00001' },
+   
+    // ... other entries ...
+  ]);
 
 
+// Cache page navigations (html) with a Network First strategy
+registerRoute(
+  // Check to see if the request is a navigation to a new page
+  ({ request }) => request.mode === 'navigate',
+  // Use a Network First caching strategy
+  new NetworkFirst({
+    // Put all cached files in a cache named 'pages'
+    cacheName: 'pages',
+    plugins: [
+      // Ensure that only requests that result in a 200 status are cached
+      new CacheableResponsePlugin({
+        statuses: [200],
+      }),
+    ],
+  }),
+);
 
+// Cache CSS, JS, and Web Worker requests with a Stale While Revalidate strategy
+registerRoute(
+  // Check to see if the request's destination is style for stylesheets, script for JavaScript, or worker for web worker
+  ({ request }) =>
+    request.destination === 'style' ||
+    request.destination === 'script' ||
+    request.destination === 'worker',
+  // Use a Stale While Revalidate caching strategy
+  new StaleWhileRevalidate({
+    // Put all cached files in a cache named 'assets'
+    cacheName: 'assets',
+    plugins: [
+      // Ensure that only requests that result in a 200 status are cached
+      new CacheableResponsePlugin({
+        statuses: [200],
+      }),
+    ],
+  }),
+);
 
-
-//             ]);
-//         })
-//     )
-// });
-
-// self.addEventListener('activate', function(event) {
-//     console.log('[Service Worker] Activating Service Worker ....', event);
-//     event.waitUntil(
-//         caches.keys()
-//         .then(function(keyList) {
-//             return Promise.all(keyList.map(function(key) {
-//                 if (key !== CACHE_STATIC_NAME && key !== CACHE_DYNAMIC_NAME) {
-//                     console.log('[Service Worker] Removing old cache.', key);
-//                     return caches.delete(key);
-//                 }
-//             }));
-//         })
-//     );
-//     return self.clients.claim();
-// });
-
-// self.addEventListener('fetch', function(event) {
-//     event.respondWith(
-//         caches.match(event.request)
-//         .then(function(response) {
-//             if (response) {
-//                 console.log(response.status)
-//                 return response;
-//             } else {
-//                 return fetch(event.request)
-//                     .then(function(res) {
-//                         return caches.open(CACHE_DYNAMIC_NAME)
-//                             .then(function(cache) {
-//                                 cache.put(event.request.url, res.clone());
-//                                 return res;
-//                             })
-//                     })
-//                     .catch(function(err) {
-//                         return caches.open(CACHE_STATIC_NAME)
-//                             .then(function(cache) {
-//                                 return cache.match('/accounts/error/');
-//                             });
-
-//                     });
-//             }
-//         })
-//     );
-// });
+// Cache images with a Cache First strategy
+registerRoute(
+  // Check to see if the request's destination is style for an image
+  ({ request }) => request.destination === 'image',
+  // Use a Cache First caching strategy
+  new CacheFirst({
+    // Put all cached files in a cache named 'images'
+    cacheName: 'images',
+    plugins: [
+      // Ensure that only requests that result in a 200 status are cached
+      new CacheableResponsePlugin({
+        statuses: [200],
+      }),
+      // Don't cache more than 50 items, and expire them after 30 days
+      new ExpirationPlugin({
+        maxEntries: 50,
+        maxAgeSeconds: 60 * 60 * 24 * 30, // 30 Days
+      }),
+    ],
+  }),
+);

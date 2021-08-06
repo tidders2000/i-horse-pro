7e396f26b0caaf0b9209279ed58c4def51c1ff62
Simon Tidman <simontidman@Simons-MBP-2.lan>
2021-08-06 10:55:58 +0100

    added allowed host

--- ihorse/settings.py
+++ ihorse/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = os.environ.get("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['i-horse.herokuapp.com', '127.0.0.1']
+ALLOWED_HOSTS = ['i-horse.herokuapp.com', '127.0.0.1','www.i-horse.co.uk']
 
 
 # Application definition
